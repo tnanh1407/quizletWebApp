d54123fae7dd317ea978b5c86ea4fdc2ca50a41c
thinhnguyen713 <nvthinhhaha@gmail.com>
2025-09-06 07:44:54 +0700

    Xóa file create-class.html

---
+++ FrontEnd/src/components/Navbar/Page/YourLibrary/LibraryChildren/ExpertSolution/ExpertSolution.jsx
@@ -0,0 +1,134 @@
+import sach from "../../../../../../assets/img/imgSach.jpg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function ExpertSolutions() {
+  const [isFilterExpertSolutions, setIsFilterExpertSolutions] = useState(false);
+
+  const toggleFilterExpertSolutions = () => {
+    setIsFilterExpertSolutions(!isFilterExpertSolutions);
+  };
+  return (
+    <div className="flashcard-set">
+      <div className="flashcardsearch">
+        <div className="flashcard-option">
+          <button className="flex" onClick={toggleFilterExpertSolutions}>
+            <p>All</p>
+            <i className="fa-solid fa-magnifying-glass"></i>
+            <div
+              className={`expertsolution ${
+                isFilterExpertSolutions ? "block" : "hidden"
+              }`}
+            >
+              <div className="folder-option">
+                <Link href="">
+                  <div className="setting-item flex">
+                    <p>All</p>
+                  </div>
+                </Link>
+                <Link href="">
+                  <div className="setting-item flex">
+                    <p>Textbooks</p>
+                  </div>
+                </Link>
+                <Link href="">
+                  <div className="setting-item flex">
+                    <p>Expert solutions</p>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </button>
+        </div>
+        <div className="flashcard-search flex">
+          <input
+            type="text"
+            placeholder="Filter"
+            className="input-research input-research-flashcard"
+          />
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </div>
+      <div className="expert-solutions">
+        <a href="">
+          <div className="popular-session flex all-section">
+            <img src={sach} alt="" />
+            <div className="popular-session-content">
+              <h1>English Grammar in Use</h1>
+              <p>4th Edition•ISBN: 9780521189088 Raymond Murphy</p>
+              <div className="popular-session-solution flex">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <p>999 solutions</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="popular-session flex all-section">
+            <img src={sach} alt="" />
+            <div className="popular-session-content">
+              <h1>English Grammar in Use</h1>
+              <p>4th Edition•ISBN: 9780521189088 Raymond Murphy</p>
+              <div className="popular-session-solution flex">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <p>999 solutions</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="popular-session flex all-section">
+            <img src={sach} alt="" />
+            <div className="popular-session-content">
+              <h1>English Grammar in Use</h1>
+              <p>4th Edition•ISBN: 9780521189088 Raymond Murphy</p>
+              <div className="popular-session-solution flex">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <p>999 solutions</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="popular-session flex all-section">
+            <img src={sach} alt="" />
+            <div className="popular-session-content">
+              <h1>English Grammar in Use</h1>
+              <p>4th Edition•ISBN: 9780521189088 Raymond Murphy</p>
+              <div className="popular-session-solution flex">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <p>999 solutions</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="popular-session flex all-section">
+            <img src={sach} alt="" />
+            <div className="popular-session-content">
+              <h1>English Grammar in Use</h1>
+              <p>4th Edition•ISBN: 9780521189088 Raymond Murphy</p>
+              <div className="popular-session-solution flex">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <p>999 solutions</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="popular-session flex all-section">
+            <img src={sach} alt="" />
+            <div className="popular-session-content">
+              <h1>English Grammar in Use</h1>
+              <p>4th Edition•ISBN: 9780521189088 Raymond Murphy</p>
+              <div className="popular-session-solution flex">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <p>999 solutions</p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ FrontEnd/src/components/Sections/SectionFlashCardCreate.jsx
@@ -0,0 +1,56 @@
+export default function SectionFlashCards (){
+    return(
+        <>
+        <div className="create-flashcard-main-content">
+
+            <div className="create-flashcard-main-content-header flex">
+                <div className="create-flashcard-main-content-header-stt">
+                    <p>1</p>
+                </div>
+                <div className="create-flashcard-main-content-header-option">
+                    <button>
+                        <div className="button-create-flashcard-main-content-grip-line">
+                            <i class="fa-solid fa-grip-lines"></i>
+                        </div>
+                    </button>
+                    <button>
+                        <div className="button-create-flashcard-main-content-delete">
+                            <i class="fa-solid fa-trash"></i>
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div className="create-flashcard-main-content-main">
+                <div className="input-term-create-flashcard-main-content-main">
+                    <input type="text" placeholder="Enter term" />
+                    <div className="option-input-term-create-flashcard-main-content-main flex">
+                        <p>TERM</p>
+                        <button>
+                            <div className="button-option-term-create-flashcard">
+                                <p>CHOOSE LANGUAGE</p>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+                <div className="input-definition-create-flashcard-main-content-main">
+                    <div className="container-input-definition">
+                        <input type="text" placeholder="Enter definition" />
+                        <div className="option-input-definition-create-flashcard-main-content-main flex">
+                            <p>DEFINITION</p>
+                            <button>
+                                <div className="button-option-definition-create-flashcard">
+                                    <p>CHOOSE LANGUAGE</p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="input-definition-create-flashcard-main-img">
+                       <i class="fa-solid fa-image"></i>
+                       <p>Image</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
