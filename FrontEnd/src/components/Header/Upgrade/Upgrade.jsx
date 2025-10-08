b2d0a1a38a5b3aa2fa983d19d43ee88c93b126e9
Kayty <khongthien2805@gmail.com>
2025-10-08 19:51:42 +0700

    Update 8/10

---
+++ FrontEnd/src/components/Header/Upgrade/Upgrade.jsx
@@ -0,0 +1,10 @@
+export default function Upgrade() {
+  return (
+    <>
+      <div className="upgrade">
+        <h1>Get better grades with the #1 learning platform</h1>
+        <div className="option-upgrade"></div>
+      </div>
+    </>
+  );
+}
