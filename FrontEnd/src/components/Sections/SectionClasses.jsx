41920767a90b1ee38354af51fe3ae0efc7a982ab
Tran Ngoc Anh <tnanh.vn@gmail.com>
2025-10-11 21:33:21 +0700

    11/10

--- FrontEnd/src/components/Sections/SectionClasses.jsx
+++ FrontEnd/src/components/Sections/SectionClasses.jsx
@@ -181,7 +181,7 @@ export default function SectionClasses() {
                         }
                       }}
                     >
-                      Join
+                      Join Class
                     </button>
                   )}
                 </>
